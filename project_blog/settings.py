806e3116467abf39ceca1675d285143b7790973a
Deepak Verma <deepakverma789@gmail.com>
2022-09-04 23:02:05 +0530

    - heroku setting

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
