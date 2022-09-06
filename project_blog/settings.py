f680562f6b772e6bd905c03d9e70fa4cdf5a508b
deepakv-ksolves <deepak.verma@ksolves.com>
2022-09-06 16:03:15 +0530

    - updated db

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'project_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djanGo',
-        'USER': 'postgres',
-        'PASSWORD': 'Dpakverma789@' if platform.system() == 'Windows' else 'root',
-        'HOST': 'localhost',
+        'NAME': 'dcblv60jghh33q',
+        'USER': 'nmhthbrnwuqnek',
+        'PASSWORD': '24145a276891413247e58e9250036683e1dd7489536be30bfe4ef06a62b67370',
+        'HOST': 'ec2-54-204-241-136.compute-1.amazonaws.com',
+        'PORT': 5432
     }
 }
 
