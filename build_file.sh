b06dcb0876b849e498ad559ae6acc16d46a3ad71
deepak_verma <deepak.verma@logicloop.io>
2023-01-17 13:09:28 +0530

    vercel deployment

--- build_file.sh
+++ build_file.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building the project..."
-python3.8.10 -m pip install -r requirements.txt
+python pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.8.10 manage.py makemigrations --noinput
-python3.8.10 manage.py migrate --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.8.10 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
