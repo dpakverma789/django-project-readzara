cd7d08cbc3fae0c2d58f82973c9f18a9f904dfc9
deepak_verma <deepak.verma@logicloop.io>
2023-01-17 13:04:17 +0530

    vercel deployment

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('readzara_key')
+# SECRET_KEY = os.environ.get('readzara_key')
+SECRET_KEY = 'django-insecure-c_2_=*0nblqrp^x03s78tqyy(qm(lzfrt80mg4q6j1mo)h3uoz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
