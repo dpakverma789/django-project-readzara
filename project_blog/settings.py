ebdc7d8673697ed868e0b7852687690040176889
deepak_verma <deepak.verma@logicloop.io>
2023-01-19 12:29:39 +0530

    vercel deployment

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -125,8 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+try:
+    STATIC_ROOT = BASE_DIR / "static"
+except:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
