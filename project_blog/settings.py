09b405a87d4f14b31f8569ce95cb76d5f55dc51b
Deepak Verma <deepak.verma@logicloop.io>
2023-02-28 12:19:10 +0530

    vercel deployment

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
