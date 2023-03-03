2a06685e5f7ff676f640061d0a0836d789142b91
Deepak Verma <deepak.verma@logicloop.io>
2023-03-03 17:42:47 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -5,3 +5,8 @@ echo "======> REQUIREMENTS INSTALLED <======"
 echo "======> COLLECTING STATIC FILES <======"
 python3.9 manage.py collectstatic --noinput --clear
 echo "======> STATIC FILES COLLECTED <======"
+
+echo "======> MAKE-MIGRATIONS <======"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "======> MAKE-MIGRATIONS-END <======"
