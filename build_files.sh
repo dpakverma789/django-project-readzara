a35a4cb718b068b5e8f9636bc1f425ea65b3da9b
Deepak Verma <deepak.verma@logicloop.io>
2023-03-03 17:31:25 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@ pip install -r requirements.txt
 echo "======> REQUIREMENTS INSTALLED <======"
 
 echo "======> COLLECTING STATIC FILES <======"
-#python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 echo "======> STATIC FILES COLLECTED <======"
