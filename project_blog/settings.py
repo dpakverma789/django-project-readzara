6a506df2b8c5a2b83ec4287d1b7ce3692e385973
Deepak Verma <deepak.verma@logicloop.io>
2023-03-04 08:19:09 +0530

    minor fix

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c_2_=*0nblqrp^x03s78tqyy(qm(lzfrt80mg4q6j1mo)h3uoz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
