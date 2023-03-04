711d0cd6ff16520b82c1a0d5db3a19eac572df6f
Deepak Verma <deepak.verma@logicloop.io>
2023-03-04 08:17:13 +0530

    minor fix

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -128,8 +128,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'staticfiles_build'/'static'
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'tmp')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
