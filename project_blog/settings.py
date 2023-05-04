b168b27a9f5655849db8d904d4e325d94e970e8e
Deepak Verma <dpakverma789@gmail.com>
2023-05-04 16:31:35 +0530

    updated db config

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'project_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djanGo',
+        'NAME': 'Read_Zara',
         'USER': 'postgres',
-        'PASSWORD': 'Dpakverma789@' if platform.system() == 'Windows' else 'root',
+        'PASSWORD': 'Dpak789@' if platform.system() == 'Windows' else 'root',
         'HOST': 'localhost',
     }
 }
