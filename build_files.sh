8103c6c04a4da0e402b4e44a063da7d3c5c130d5
deepak verma <deepak.verma@logicloop.io>
2025-03-07 14:52:15 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
-python -m ensurepip --default-pip
-#pip install --upgrade pip
 echo "======> INSTALLING REQUIREMENTS <======"
-#pip install -r requirements.txt
-python -m pip install -r requirements.txt
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+pip install -r requirements.txt
 echo "======> REQUIREMENTS INSTALLED <======"
 
 echo "======> COLLECTING STATIC FILES <======"
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput
 echo "======> STATIC FILES COLLECTED <======"
 
 echo "======> MAKE-MIGRATIONS <======"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 echo "======> MAKE-MIGRATIONS-END <======"
